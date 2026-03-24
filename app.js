414dc3b7e08761354471bd09fec7da5a9c9c1e05
BaroiuClaudiu <claudiu.baroiu@student.usv.ro>
2026-03-24 14:01:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -44,11 +44,35 @@ let currentPosition = null;
 navigator.geolocation.getCurrentPosition((pos) => {
   currentPosition = [pos.coords.latitude, pos.coords.longitude];
   map.setView(currentPosition, 15);
+  L.circle(currentPosition, {
+    radius: 2000, // metri
+    color: "blue",
+    fillOpacity: 0.1,
+  }).addTo(map);
+  loadReports();
 });
 
-// Adaugă report
 window.addReport = async function () {
-  if (!selectedPosition) return alert("Selectează un punct pe hartă!");
+  if (!selectedPosition) {
+    return alert("Selectează un punct pe hartă!");
+  }
+
+  if (!currentPosition) {
+    return alert("Nu avem locația ta!");
+  }
+
+  const distance = getDistance(
+    currentPosition[0],
+    currentPosition[1],
+    selectedPosition[0],
+    selectedPosition[1],
+  );
+
+  if (distance > 2) {
+    return alert(
+      "Poți adăuga evenimente doar pe o rază de 2 km de la locația ta!",
+    );
+  }
 
   const type = document.getElementById("type").value;
 
@@ -60,7 +84,7 @@ window.addReport = async function () {
     timestamp: Date.now(),
   });
 
-  alert("Adăugat!");
+  alert("Eveniment adăugat!");
   loadReports();
 };
 
@@ -71,11 +95,20 @@ async function loadReports() {
   querySnapshot.forEach((doc) => {
     const data = doc.data();
 
-    L.marker([data.lat, data.lng], {
-      icon: getIcon(data.type),
-    })
-      .addTo(map)
-      .bindPopup(`${data.type} | Votes: ${data.votes}`);
+    const distance = getDistance(
+      currentPosition[0],
+      currentPosition[1],
+      data.lat,
+      data.lng,
+    );
+
+    if (distance <= 2) {
+      L.marker([data.lat, data.lng], {
+        icon: getIcon(data.type),
+      })
+        .addTo(map)
+        .bindPopup(`${data.type} | ${distance.toFixed(2)} km`);
+    }
   });
 }
 function getIcon(type) {
@@ -90,4 +123,18 @@ function getIcon(type) {
       return accidentIcon;
   }
 }
-loadReports();
+function getDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371; // km
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+}
